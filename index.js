89bed43e525d2ecdb657d97d0f0d45bb43d3e4fb
Daniel Sinkpon <danielsinkpon@gmail.com>
2025-06-15 11:37:37 -0400

    Publish to the world the Expanding Cards project
    Create markup with HTML boilerplate, different text
    and images.
    
    Style each card to be all shrinked except for one
    
    Implement logic to expand a card when it is clicked
    by the user while the other cards shrink.

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const imagesContainer = document.querySelector('.images__container');
 const allImages = document.querySelectorAll('.image__container');
 function selectImage(e){
+
+    // If what we clicked was not a card, do nothing
+    if (e.target.className !== 'image__container') return;
+
+    // For each card
     allImages.forEach(ele => {
-        if (ele.classList.contains("active")){
-            ele.classList.remove('active');
-        }
+
+        // If it is the card we clicked
         if (ele.id === e.target.id) {
-            ele.classList.add('active');
+
+            // And it wasn't already expanded
+            if (!ele.classList.contains("active")){
+
+                // Expand it
+                ele.classList.add('active');
+            } // Otherwise, do nothing
+
+            // Go to the next card
+            return;
+        }
+
+        // If it wasn't the card we clicked
+
+        // And the card was already expanded
+        if (ele.classList.contains("active")){
+
+            // Shrink it
+            ele.classList.remove('active');
         }
     })
 }
